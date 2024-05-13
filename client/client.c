582e219722fec109604c6b0fbceb4faca4897d5f
nath54 <cerisara.nathan@gmail.com>
2024-05-13 16:29:02 +0200

    Add polling client

--- client/client.c
+++ client/client.c
@@ -1,94 +1,116 @@
-
-/*
-
-Code suivant à adapter:
-    il y a base recepteur TCP
-
-*/
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/poll.h>
 #include <sys/errno.h>
-#include <sys/ioctl.h>
-#include <sys/time.h>
-
 #include <netinet/in.h>
-
 #include <arpa/inet.h>
-
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-
 #define INVALID_SOCKET -1
 #define SOCKET_ERROR -1
-
+#define BUFFER_SIZE 1024
 
 typedef int SOCKET;
 typedef struct sockaddr_in SOCKADDR_IN;
 typedef struct sockaddr SOCKADDR;
-typedef struct in_addr IN_ADDR;
 
+int main(int argc, char* argv[]) {
+  // Socket descriptor
+  SOCKET sockfd;
+  // Server address structure
+  SOCKADDR_IN dest = {0};
+  // Buffer for received data
+  char buffer[BUFFER_SIZE];
+  // File descriptor for standard input
+  int stdin_fd = fileno(stdin);
 
-int main(int argc, char **argv)
-{
-    // descripteur de socket
-    SOCKET sockfd;
-    // structure d'adresse qui contiendra les
-    SOCKADDR_IN dest = {0};
+  // Check arguments
+  if (argc != 3) {
+    printf("Usage: %s ip_proxy port_proxy\n", argv[0]);
+    exit(-1);
+  }
 
-    // parametres reseaux du destinataire
+  char* ip_proxy = argv[1];
+  int port_proxy = atoi(argv[2]);
 
-    // verification du nombre d'arguments sur la ligne de commande
-    if(argc != 4) {
-        printf("Usage : %s @dest num_port chaine_a_envoyer\n", argv[0]);
-        exit(-1);
+  // Create socket
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == INVALID_SOCKET) {
+    perror("socket");
+    exit(errno);
+  }
+
+  // Initialize server address
+  dest.sin_family = AF_INET;
+  inet_aton(ip_proxy, &dest.sin_addr);
+  dest.sin_port = htons(port_proxy);
+
+  // Connect to server
+  if (connect(sockfd, (SOCKADDR*)&dest, sizeof(SOCKADDR)) == SOCKET_ERROR) {
+    perror("connect");
+    exit(errno);
+  }
+
+  // Create poll structure
+  struct pollfd fds[2];
+  fds[0].fd = sockfd;
+  fds[0].events = POLLIN; // Interested in readability on socket
+  fds[1].fd = stdin_fd;
+  fds[1].events = POLLIN; // Interested in readability on standard input
+
+  while (1) {
+    int ret = poll(fds, 2, -1); // Wait indefinitely
+    if (ret == -1) {
+      perror("poll");
+      break;
+    } else if (ret == 0) {
+      // Timeout (shouldn't happen here)
+      continue;
     }
 
-    char* addr = argv[1];
-    int port = atoi(argv[2]);
-    char* msg =  argv[3];
-    unsigned int tmsg = strlen(msg);
-
-    // creation de la socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    if(sockfd == INVALID_SOCKET){
-        fprintf(stderr, "Erreur lors de la création du socket!\n");
-        exit(errno);
-    }
-
-    // initialisation de la structure d'adresse du destinataire :
-
-    // famille d'adresse
-    dest.sin_family = AF_INET;
-
-    // adresse IPv4 du destinataire
-    inet_aton(addr , &(dest.sin_addr));
-
-    // port du destinataire
-    dest.sin_port = htons(port);
-
-    //
-    if(connect(sockfd,(SOCKADDR *) &dest, sizeof(SOCKADDR)) == SOCKET_ERROR)
-    {
-        perror("connect()");
-        exit(errno);
-    }
-
-
-    // envoi de la chaine
-    if(sendto(sockfd, msg, tmsg, 0, (SOCKADDR *)&dest, sizeof(dest)) == -1)
-      {
-        perror("erreur a l'appel de la fonction sendto -> ");
-        exit(errno);
+    // Check for activity on socket
+    if (fds[0].revents & POLLIN) {
+      int bytes_received = recv(sockfd, buffer, BUFFER_SIZE, 0);
+      if (bytes_received == 0) {
+        printf("Server closed connection\n");
+        break;
+      } else if (bytes_received == -1) {
+        perror("recv");
+        break;
       }
 
-    // fermeture de la socket
-    close(sockfd);
+      // Process received data
+      printf("Received from server: %s\n", buffer);
+    }
 
-    return 0;
+    // Check for user input
+    if (fds[1].revents & POLLIN) {
+      // Read message from standard input
+      int bytes_read = read(stdin_fd, buffer, BUFFER_SIZE);
+      if (bytes_read == 0) {
+        printf("User closed input\n");
+        break;
+      } else if (bytes_read == -1) {
+        perror("read");
+        break;
+      }
+
+      // Send message to server
+      buffer[bytes_read - 1] = '\0'; // Replace newline with null terminator
+      int bytes_sent = send(sockfd, buffer, bytes_read, 0);
+      if (bytes_sent == -1) {
+        perror("send");
+        break;
+      }
+    }
+  }
+
+  // Close socket
+  close(sockfd);
+
+  return 0;
 }
