fbee0a58c045a0a5fb14397b283e5cea53685aa0
nath54 <cerisara.nathan@gmail.com>
2024-05-13 15:23:26 +0200

    ajouts codes de base projets

--- client/client.c
+++ client/client.c
@@ -1,1 +1,90 @@
-//TODO
+
+/*
+
+Code suivant à adapter:
+    il y a base recepteur TCP
+
+*/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/poll.h>
+#include <sys/errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
+
+
+typedef int SOCKET;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+typedef struct in_addr IN_ADDR;
+
+
+int main(int argc, char **argv)
+{
+    // descripteur de socket
+    SOCKET sockfd;
+    // structure d'adresse qui contiendra les
+    SOCKADDR_IN dest = {0};
+
+    // parametres reseaux du destinataire
+
+    // verification du nombre d'arguments sur la ligne de commande
+    if(argc != 4) {
+        printf("Usage : %s @dest num_port chaine_a_envoyer\n", argv[0]);
+        exit(-1);
+    }
+
+    char* addr = argv[1];
+    int port = atoi(argv[2]);
+    char* msg =  argv[3];
+    unsigned int tmsg = strlen(msg);
+
+    // creation de la socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if(sockfd == INVALID_SOCKET){
+        fprintf(stderr, "Erreur lors de la création du socket!\n");
+        exit(errno);
+    }
+
+    // initialisation de la structure d'adresse du destinataire :
+
+    // famille d'adresse
+    dest.sin_family = AF_INET;
+
+    // adresse IPv4 du destinataire
+    inet_aton(addr , &(dest.sin_addr));
+
+    // port du destinataire
+    dest.sin_port = htons(port);
+
+    //
+    if(connect(sockfd,(SOCKADDR *) &dest, sizeof(SOCKADDR)) == SOCKET_ERROR)
+    {
+        perror("connect()");
+        exit(errno);
+    }
+
+
+    // envoi de la chaine
+    if(sendto(sockfd, msg, tmsg, 0, (SOCKADDR *)&dest, sizeof(dest)) == -1)
+      {
+        perror("erreur a l'appel de la fonction sendto -> ");
+        exit(errno);
+      }
+
+    // fermeture de la socket
+    close(sockfd);
+
+    return 0;
+}
--- serveur/serveur.c
+++ serveur/serveur.c
@@ -1,1 +1,113 @@
-//TODO
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/poll.h>
+#include <sys/errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
+
+
+typedef int SOCKET;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+typedef struct in_addr IN_ADDR;
+
+#define TAILLE_BUF 1024
+
+
+/*
+
+Code suivant à adapter:
+    il y a base recepteur TCP
+
+*/
+
+int main(int argc, char **argv)
+{
+    // descripteur de socket
+    SOCKET sockfd;
+    // espace necessaire pour stocker le message recu
+    char buf[TAILLE_BUF];
+
+    // taille d'une structure sockaddr_in utile pour la fonction recvfrom
+    socklen_t fromlen = sizeof(SOCKADDR_IN); 
+
+    // structure d'adresse qui contiendra les param reseaux du recepteur
+    SOCKADDR_IN my_addr;
+    // structure d'adresse qui contiendra les param reseaux de l'expediteur
+    SOCKADDR_IN client;
+
+    SOCKET acc_sockfd;
+
+    // verification du nombre d'arguments sur la ligne de commande
+    if(argc != 2)
+    {
+        printf("Usage: %s port_local\n", argv[0]);
+        exit(-1);
+    }
+
+    int port = atoi(argv[1]);
+
+    // creation de la socket
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(sockfd == INVALID_SOCKET){
+        fprintf(stderr, "Erreur lors de la création du socket!\n");
+        exit(errno);
+    }
+
+    // initialisation de la structure d'adresse du recepteur (pg local)
+
+    // nous sommes un serveur, nous acceptons n'importe quelle adresse
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // famille d'adresse
+    my_addr.sin_family = AF_INET;
+
+    // recuperation du port du recepteur
+    my_addr.sin_port = htons(port);
+
+    // adresse IPv4 du recepteur
+    inet_aton("127.0.0.1", &(my_addr.sin_addr));
+
+    // association de la socket et des param reseaux du recepteur
+    if(bind(sockfd, (SOCKADDR *)&my_addr, sizeof(my_addr)) != 0)
+      {
+        perror("erreur lors de l'appel a bind -> ");
+        exit(errno);
+      }
+
+    if(listen(sockfd, 5) == SOCKET_ERROR)
+    {
+        perror("listen()");
+        exit(errno);
+    }
+
+    acc_sockfd = accept(sockfd, (SOCKADDR *)&my_addr, &fromlen);
+
+    // reception de la chaine de caracteres
+    if( recvfrom(acc_sockfd, buf, TAILLE_BUF, 0,
+                    (SOCKADDR *)&my_addr, &fromlen ) == -1)
+    {
+        perror("erreur de reception -> ");
+        exit(errno);
+    }
+
+    // affichage de la chaine de caracteres recue
+    printf("received from socket : \"%s\"\n", buf);
+
+    // fermeture de la socket
+    close(acc_sockfd);
+    close(sockfd);
+
+    return 0;
+}
