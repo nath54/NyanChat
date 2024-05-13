3f7905329741e4e2b6a4b1561f970edc7aa22837
nath54 <cerisara.nathan@gmail.com>
2024-05-13 16:18:00 +0200

    AAAAAAAAh, plus de franglais

--- proxy/proxy.c
+++ proxy/proxy.c
@@ -1,1 +1,27 @@
-//TODO
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/poll.h>
+#include <sys/errno.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+
+#include <netinet/in.h>
+
+#include <arpa/inet.h>
+
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+
+
+
+
+
+
+
+
--- server/server.c
+++ server/server.c
@@ -2,14 +2,19 @@
 #include <sys/socket.h>
 #include <sys/poll.h>
 #include <sys/errno.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+
+#include <netinet/in.h>
+
+#include <arpa/inet.h>
+
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+
 
 #define INVALID_SOCKET -1
 #define SOCKET_ERROR -1
