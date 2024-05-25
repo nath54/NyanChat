b32c405c5a67d23ec3f603b7cf34c194efe2b9b2
Ysée J <ysee.jacquet@etu.unistra.fr>
2024-05-25 14:32:20 +0200

    update comments

--- include/code_errors.h
+++ include/code_errors.h
@@ -12,15 +12,10 @@
 extern double BIT_ERROR_RATE; // in %
 
 
-/**
- * Renvoie un mot du code sur 8 + c bits (c étant le degré de votre polynôme),
- * rajoutant au mot m les bits de parité décrit par la matrice G dérivée de votre code polynomial.*
- * Le mot à encoder (sur 8 bits) sera placé sur les premiers bits de la variable m et complété
- * par 8 bits de padding avant d’être fourni en argument à la fonction.
- * Vous privilégierez des opérateurs bit à bit en évitant les opérations arithmétiques.
-*/
+// Encode a word `m` with the generator matrix `G`.
 uint16_t encode(uint16_t G[K][N], uint16_t m);
 
+// Decode a word `m` with the parity-check matrix `H`.
 uint8_t decode(uint16_t H[C][N], uint16_t m);
 
 void add_control_bits(Message *msg);
@@ -39,11 +34,13 @@ by the polynomial `p` using the shift register method.
 */
 uint8_t rem_lfsr(uint16_t p, uint16_t x);
 
-/**
- * Encode a 8-bit word with polynomial generator P using LFSR
-*/
+// Encode a 16-bit word `m` with polynomial generator `P` using LFSR
 uint16_t encode_lfsr(uint16_t P, uint16_t m);
 
+/*
+Decode a 16-bit word `m` with polynomial generator `P` using LFSR
+and return the decoded 8-bit word.
+*/
 uint8_t decode_lfsr(uint16_t P, uint16_t m);
 
 // Fill the generator matrix `G` from the polynomial `p`.
